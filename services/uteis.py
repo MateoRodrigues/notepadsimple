35a9500f0e83b2eb5c10ec2aaa169adb122a8af8
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-04-03 18:49:04 -0300

    mudando título quando abrir arquivo ainda não funciona

--- services/uteis.py
+++ services/uteis.py
@@ -1,13 +1,4 @@
 import tkinter as tk
-from services.filetxt import FileTxt
-def caixa_para_arquivo(caixa_texto: tk.Text):
-    arquivo_texto =caixa_texto.get("1.0", tk.END)
-    return arquivo_texto
-
-def arquivo_para_caixa():
-    pass
-
-
 
 def titulo_pagina(titulo_da_pagina="Sem título - Bloco de Notas"):
     return titulo_da_pagina
