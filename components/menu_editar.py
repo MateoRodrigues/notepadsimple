64aab7caa70a2bc620238d25c0341d83d1564927
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-04-10 17:27:49 -0300

    incluindo corretamente o menu editar sem nenhuma função

--- components/menu_editar.py
+++ components/menu_editar.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 
 class MenuEditar(tk.Menu):
-    def __init__(self, master=None, caixa_texto=None):      
+    def __init__(self, master=None):      
         super().__init__(master, tearoff=0)
-        self.caixa_texto = caixa_texto
         self.add_cascade(label='Editar')
-        self.add_command(label='Copiar', command=self.copiar)
-        self.add_command(label='Colar', command=self.colar)
-        self.add_command(label='Recortar', command=self.recortar)
-        self.add_command(label='Limpar', command=self.limpar)
-        self.add_command(label='Selecionar Tudo', command=self.selecionar_tudo)
+        self.add_command(label='Copiar')
+        self.add_command(label='Colar')
+        self.add_command(label='Recortar')
+        self.add_command(label='Limpar')
+        self.add_command(label='Selecionar Tudo')
--- components/menu_principal.py
+++ components/menu_principal.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from .menu_arquivo import MenuArquivo
+from .menu_editar import MenuEditar
 
 class Menu(tk.Menu):
     def __init__(self,master=None, caixa_texto=None):
@@ -7,6 +8,6 @@ class Menu(tk.Menu):
         self.master = master
         self.caixa_texto = caixa_texto  
         menu_arquivo1 = MenuArquivo(self, caixa_texto=caixa_texto)
-        menu_editar1 = tk.Menu(self, tearoff=0)
+        menu_editar1 = MenuEditar(self)
         self.add_cascade(label="Arquivo",menu=menu_arquivo1)
         self.add_cascade(label="Editar",menu=menu_editar1)
