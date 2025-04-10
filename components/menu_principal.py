e12fee848e7a03075811938fcab40b0245e19c2d
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-04-10 17:19:09 -0300

    incorporando o menu editar em menu principal

--- components/menu_principal.py
+++ components/menu_principal.py
@@ -7,4 +7,6 @@ class Menu(tk.Menu):
         self.master = master
         self.caixa_texto = caixa_texto  
         menu_arquivo1 = MenuArquivo(self, caixa_texto=caixa_texto)
+        menu_editar1 = tk.Menu(self, tearoff=0)
         self.add_cascade(label="Arquivo",menu=menu_arquivo1)
+        self.add_cascade(label="Editar",menu=menu_editar1)
