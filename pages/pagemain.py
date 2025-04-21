9771cc3615b62fb084a73fcc6e1cd11c5fc9dff8
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-04-21 12:11:16 -0300

    mudando a fonte da caixa de texto

--- pages/pagemain.py
+++ pages/pagemain.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font as tkFont
 from components.menu_principal import Menu
 from components.caixa_texto import CaixaTexto
 from services.uteis import titulo_pagina
@@ -6,10 +7,13 @@ class JanelaPrincipal(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry("1000x500")
+        self.fonte_menu = tkFont.Font(family="Segoe UI", size=10)
+        self.option_add("*Menu*Font", self.fonte_menu)
         # Buscando o ícone nos arquivos
         self.iconbitmap("C:/Users/mateo/Documents/python/notepadsimple/assets/Double-J-Design-Origami-Notepad.ico")
         self.title(titulo_pagina())
-        self.caixa_texto = CaixaTexto(self)
+        self.fonte_caixa_texto = tkFont.Font(family="Segoe UI", size=12)
+        self.caixa_texto = CaixaTexto(self, font=self.fonte_caixa_texto)
         self.menu = Menu(self, self.caixa_texto)
         self.config(menu=self.menu)
         self.caixa_texto.pack(side=tk.BOTTOM,expand=True,fill='both')
