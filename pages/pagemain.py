9d9eff40d93db2d80f3941c502c57642fa96de12
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-07-22 17:41:30 -0300

    icone linux

--- pages/pagemain.py
+++ pages/pagemain.py
@@ -1,8 +1,10 @@
+import os
 import tkinter as tk
 import tkinter.font as tkFont
 from components.menu_principal import Menu
 from components.caixa_texto import CaixaTexto
 from services.uteis import titulo_pagina
+
 class JanelaPrincipal(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -15,7 +17,11 @@ class JanelaPrincipal(tk.Tk):
         self.option_add("*Menu*Font", self.fonte_menu)
 
         # Buscando o ícone nos arquivos
-        #icon = tk.PhotoImage(file="/assets/icone.png")
+        caminho_base = os.path.dirname(__file__)
+        caminho_icone = os.path.join(caminho_base, "../assets/iconelinux.png")
+        icon = tk.PhotoImage(file=caminho_icone)
+        self.iconphoto(True, icon)
+        #Titulo da página
         self.title(titulo_pagina())
 
         # Configurações da caixa de texto
