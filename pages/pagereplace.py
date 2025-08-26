f7c5490c5f45a01c394f517227e1111fe773f8b9
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-08-26 18:06:12 -0300

    componente labels e entry colocados na janela substituir

--- pages/pagereplace.py
+++ pages/pagereplace.py
@@ -3,15 +3,17 @@ import customtkinter as ctk
 class JanelaSubstituir(ctk.CTkToplevel):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs)
+        self.frame = ctk.CTkFrame(self)
+        self.frame.pack(pady=0, padx=0, side=ctk.TOP, fill="x")
 
         self.title("Substituir")
         self.geometry("600x300")
 
-        self.label = ctk.CTkLabel(self, text="Sou uma janela do CustomTkinter")
-        self.label.pack(pady=20)
+        self.label = ctk.CTkLabel(self.frame, text="Localizar:")
+        self.label.pack(pady=20, side = ctk.LEFT)
 
-        self.entry = ctk.CTkEntry(self, placeholder_text="Digite algo...")
-        self.entry.pack(pady=5)
+        self.entry = ctk.CTkEntry(self.frame, placeholder_text="Digite algo...")
+        self.entry.pack(padx=10, side = ctk.LEFT)
 
-        self.button = ctk.CTkButton(self, text="Fechar", command=self.destroy)
-        self.button.pack(pady=10)
+        self.button = ctk.CTkButton(self.frame, text="Substituir")
+        self.button.pack(pady=10,side = ctk.LEFT)
