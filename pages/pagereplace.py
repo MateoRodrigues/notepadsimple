b0789b040e356f75a79163d777c3bf6601aa2c5a
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-09-02 17:31:39 -0300

    atualizando pagereplace

--- pages/pagereplace.py
+++ pages/pagereplace.py
@@ -17,7 +17,7 @@ class JanelaSubstituir(ctk.CTkToplevel):
         self.entry = ctk.CTkEntry(self.frame)
         self.entry.pack(padx=5, side = ctk.LEFT)
         self.button = ctk.CTkButton(self.frame, text="Localizar Próxima")
-        self.button.pack(pady=10,side = ctk.LEFT)
+        self.button.pack(pady=20,side = ctk.LEFT)
 
         # Configurações do frame de substituir
         self.label_substituir = ctk.CTkLabel(self.frame_substituir, text="Substituir:")
@@ -25,8 +25,8 @@ class JanelaSubstituir(ctk.CTkToplevel):
         self.entry_substituir = ctk.CTkEntry(self.frame_substituir)
         self.entry_substituir.pack(padx=5, side = ctk.LEFT)
         self.button_subs = ctk.CTkButton(self.frame_substituir, text="Substituir")
-        #self.button_subs_all = ctk.CTkButton(self.frame_substituir, text="Substituir Tudo")
-        #self.button_subs_all.pack(padx=100,pady=10,side = ctk.BOTTOM)
-        self.button_subs.pack(pady=10,side = ctk.LEFT)
+        self.button_subs_all = ctk.CTkButton(self.frame_substituir, text="Substituir Tudo")
+        self.button_subs_all.pack(padx=100, side = ctk.RIGHT)
+        self.button_subs.pack(pady=0,side = ctk.LEFT)
 
 
