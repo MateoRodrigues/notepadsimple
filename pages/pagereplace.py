b364dc04b8e39135807c6e3ce844325f8fb9a382
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-09-02 17:16:44 -0300

    adicionando elementos na janela substituir

--- pages/pagereplace.py
+++ pages/pagereplace.py
@@ -4,16 +4,29 @@ class JanelaSubstituir(ctk.CTkToplevel):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs)
         self.frame = ctk.CTkFrame(self)
+        self.frame_substituir = ctk.CTkFrame(self)
         self.frame.pack(pady=0, padx=0, side=ctk.TOP, fill="x")
+        self.frame_substituir.pack(pady=0, padx=0, side=ctk.TOP, fill="x")
 
         self.title("Substituir")
         self.geometry("600x300")
 
+        # Configurações do frame
         self.label = ctk.CTkLabel(self.frame, text="Localizar:")
-        self.label.pack(pady=20, side = ctk.LEFT)
-
-        self.entry = ctk.CTkEntry(self.frame, placeholder_text="Digite algo...")
-        self.entry.pack(padx=10, side = ctk.LEFT)
-
-        self.button = ctk.CTkButton(self.frame, text="Substituir")
+        self.label.pack(padx=5,pady=20, side = ctk.LEFT)
+        self.entry = ctk.CTkEntry(self.frame)
+        self.entry.pack(padx=5, side = ctk.LEFT)
+        self.button = ctk.CTkButton(self.frame, text="Localizar Próxima")
         self.button.pack(pady=10,side = ctk.LEFT)
+
+        # Configurações do frame de substituir
+        self.label_substituir = ctk.CTkLabel(self.frame_substituir, text="Substituir:")
+        self.label_substituir.pack(padx=5,pady=20, side = ctk.LEFT)
+        self.entry_substituir = ctk.CTkEntry(self.frame_substituir)
+        self.entry_substituir.pack(padx=5, side = ctk.LEFT)
+        self.button_subs = ctk.CTkButton(self.frame_substituir, text="Substituir")
+        #self.button_subs_all = ctk.CTkButton(self.frame_substituir, text="Substituir Tudo")
+        #self.button_subs_all.pack(padx=100,pady=10,side = ctk.BOTTOM)
+        self.button_subs.pack(pady=10,side = ctk.LEFT)
+
+
