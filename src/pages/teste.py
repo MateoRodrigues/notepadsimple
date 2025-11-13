4c87b1f8c17aca8e365eda4fdb4ff6a0c8c14cef
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-11-13 14:30:31 -0300

    removendo o arquivo src/pages/teste.py

--- src/pages/teste.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-import tkinter.font as tkFont
-
-# Adicione isso no seu __init__ após criar o menu:
-fonte_atual = tkFont.nametofont("TkDefaultFont")
-print(f"Fonte do sistema: {fonte_atual.actual()}")
