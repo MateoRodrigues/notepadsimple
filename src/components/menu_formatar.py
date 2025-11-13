92f14c03bf4580ba08596030de4d1b2c7cdbef6f
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-11-13 14:55:19 -0300

    retirando um atributo label que continha o valor string formatar.

--- src/components/menu_formatar.py
+++ src/components/menu_formatar.py
@@ -6,7 +6,6 @@ class MenuFormatar(tk.Menu):
         super().__init__(master, tearoff=0,bg='white',activebackground="lightblue",activeforeground="black")
         self.janelasubstituir = JanelaSubstituir
         self.pagina = pagina
-        self.add_cascade(label='Formatar')
         self.add_command(label='Fonte...')
         self.add_command(label='Codificar')
         self.add_command(label='Substituir', command=self.substituir)
