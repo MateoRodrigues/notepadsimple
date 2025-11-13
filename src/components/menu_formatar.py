077c3ed438a9d887fe4b24bee7b2d906596589de
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-11-13 16:45:04 -0300

    modificando para exibir fontes do tkinter.

--- src/components/menu_formatar.py
+++ src/components/menu_formatar.py
@@ -4,20 +4,17 @@ from pages.pagereplace import JanelaSubstituir
 
 class MenuFormatar(tk.Menu):
     def __init__(self, master=None, pagina=None):      
-        super().__init__(master, tearoff=0,bg='white',activebackground="lightblue",activeforeground="black")
+        super().__init__(master, tearoff=0)
         self.janelasubstituir = JanelaSubstituir
         self.pagina = pagina
         self.menu_fonte = tk.Menu(self, tearoff=0)
         for fonte in sorted(tkFont.families()):
-            self.menu_fonte.add_command(label=fonte, command=self.alterar_fonte)
+            self.menu_fonte.add_command(label=fonte, command=lambda f=fonte: self.alterar_fonte(f))
         self.add_cascade(label='Fonte...', menu=self.menu_fonte)
-        self.add_command(label='Codificar')
         self.add_command(label='Substituir', command=self.substituir)
 
-    def substituir(self):
-        self.janelasubstituir(self.pagina)
-    def alterar_fonte(self):
-        pass
+    def alterar_fonte(self, fonte):
+        print(fonte)
 
 
    
