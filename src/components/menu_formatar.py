60080031564d02cb67f6cf13944335b1e53a8031
MateoRodrigues <matheus.machado.curso@gmail.com>
2026-01-19 17:43:06 -0300

    retira o a importação da janela substituir, o parâmetro self.janelasubstituir, a opção do janela substituir e a função substituir.

--- src/components/menu_formatar.py
+++ src/components/menu_formatar.py
@@ -1,12 +1,10 @@
 import tkinter as tk
 import tkinter.font as tkFont
-from pages.pagereplace import JanelaSubstituir
 
 
 class MenuFormatar(tk.Menu):
     def __init__(self, master=None, pagina=None):
         super().__init__(master, tearoff=0)
-        self.janelasubstituir = JanelaSubstituir
         self.pagina = pagina
         self.menu_fonte = tk.Menu(self, tearoff=0)
         # Adiciona as fontes disponíveis ao submenu de fontes
@@ -16,10 +14,7 @@ class MenuFormatar(tk.Menu):
                 label=fonte, command=lambda f=fonte: self.alterar_fonte(f)
             )
         self.add_cascade(label="Fonte...", menu=self.menu_fonte)
-        self.add_command(label="Substituir", command=self.substituir)
 
     def alterar_fonte(self, fonte):
         print(fonte)
 
-    def substituir(self):
-        self.janelasubstituir(self.pagina)
